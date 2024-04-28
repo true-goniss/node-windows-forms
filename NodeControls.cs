2a6a8ededc1dfb55d21c5089bf01171e2137ca3e
true-goniss <goniss9801@gmail.com>
2024-04-29 01:58:55 +0300

    saving generated socket port to tmp/socketPort.dat. loading socket port from there if file exists

--- NodeControls.cs
+++ NodeControls.cs
@@ -31,7 +31,15 @@ static class NodeControls
         eventEmittersJS = "";
         usedNames = "";
 
-        socketPort = FindFreePort();
+        string socketPortFilePath = Path.Combine(outputPath, "tmp", "socketPort.dat");
+        socketPort = await FileSystemFuncs.FileReadInt(socketPortFilePath);
+        
+        if(socketPort == 0)
+        {
+            socketPort = FindFreePort();
+        }
+
+        FileSystemFuncs.FileWriteInt(socketPortFilePath, socketPort);
 
         if (websocket == null)
         {
@@ -1270,5 +1278,82 @@ static class CsharpEventHandlers
         {
             System.Diagnostics.Debug.WriteLine("\"" + eventName + "\"" + ",");
         }
+    }
+}
+
+static class FileSystemFuncs
+{
+    public static async Task<int> FileReadInt(string path)
+    {
+        string text = await FileReadAllText(path);
+
+        if (text == "")
+        {
+            return 0;
+        }
+        else
+        {
+            try
+            {
+                return Convert.ToInt32(text);
+            }
+            catch (Exception)
+            {
+                return 0;
+            }
+        }
+    }
+
+    public static async Task<bool> FileWriteInt(string path, int value)
+    {
+        return await FileWriteAllText(path, value.ToString());
+    }
+
+    static async Task<bool> FileWriteAllText(string path, string content)
+    {
+        bool accessSuccess = false;
+        do
+        {
+            string directory = (new System.IO.DirectoryInfo(path)).Parent.FullName;
+            System.IO.Directory.CreateDirectory(directory);
+
+            try
+            {
+                System.IO.File.WriteAllText(path, content);
+                accessSuccess = true;
+            }
+            catch (Exception ex) { await Task.Delay(100); }
+        }
+        while (!accessSuccess);
+
+        return accessSuccess;
+    }
+
+    static async Task<string> FileReadAllText(string path)
+    {
+        if (System.IO.File.Exists(path))
+        {
+            bool accessSuccess = false;
+            do
+            {
+                EnsureFileDirectory(path);
+
+                try
+                {
+                    string res = File.ReadAllText(path);
+                    return res;
+                }
+                catch (Exception ex) { await Task.Delay(100); }
+            }
+            while (!accessSuccess);
+        }
+
+        return "";
+    }
+
+    static void EnsureFileDirectory(string filePath)
+    {
+        string directory = (new System.IO.DirectoryInfo(filePath)).Parent.FullName;
+        System.IO.Directory.CreateDirectory(directory);
     }
 }
